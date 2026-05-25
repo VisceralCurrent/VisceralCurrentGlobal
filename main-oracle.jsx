af06158a0c151fff9682351c1fe7bbf62bf864f1
Chris Van Houten <chrisvanhouten@students.suu.edu>
2026-05-25 11:38:54 -0600

    Delete main-oracle.jsx

--- main-oracle.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import OracleApp from './OracleApp.jsx'
-
-ReactDOM.createRoot(document.getElementById('root-oracle')).render(
-  <OracleApp />
-)
