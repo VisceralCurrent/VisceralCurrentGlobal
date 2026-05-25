f6312fbfb352ae64a4ad478e05572ab114eccc00
Chris Van Houten <chrisvanhouten@students.suu.edu>
2026-05-25 11:39:14 -0600

    Delete main-research.jsx

--- main-research.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './ResearchApp.jsx'
-
-const rootElement = document.getElementById('root-app');
-if (rootElement) {
-    ReactDOM.createRoot(rootElement).render(<App />);
-}
