172a351485389a2f9d77f4478a6eb67f3492cdd9
Chris Van Houten <chrisvanhouten@students.suu.edu>
2026-05-25 11:38:42 -0600

    Delete main-dashboard.jsx

--- main-dashboard.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import DashboardApp from './DashboardApp.jsx'
-
-const rootElement = document.getElementById('portal-root');
-if (rootElement) {
-    ReactDOM.createRoot(rootElement).render(<DashboardApp />);
-}
